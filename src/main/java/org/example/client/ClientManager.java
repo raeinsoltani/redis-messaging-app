a5b5f93c337cbd659ff1affe52dcebef41a8da0a
Raein Soltani <raeen.soltani@yahoo.com>
2023-03-09 00:04:43 +0330

    feat: created client package classes

---
+++ src/main/java/org/example/client/ClientManager.java
@@ -0,0 +1,16 @@
+package org.example.client;
+
+public class ClientManager {
+    private String ip;
+    private int port;
+    private String username;
+
+    public ClientManager(String ip, int port ,String username) {
+        this.ip = ip;
+        this.port = port;
+        this.username = username;
+    }
+
+
+
+}
