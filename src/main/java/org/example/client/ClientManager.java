61244eb13998b32968e20401bec21fd59c7029c9
Raein Soltani <raeen.soltani@yahoo.com>
2023-03-10 02:21:12 +0330

    feat: created login and createGroup method on the client side

--- src/main/java/org/example/client/ClientManager.java
+++ src/main/java/org/example/client/ClientManager.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ClientManager {
@@ -18,22 +19,21 @@ public class ClientManager {
         this.port = port;
     }
 
+    Scanner scanner;
+    ObjectOutputStream outputStream;
+    Socket socket;
+
     public void start(){
         try {
-            Socket socket = new Socket(ip,port);
-            ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+            socket = new Socket(ip,port);
+            outputStream = new ObjectOutputStream(socket.getOutputStream());
 
             Listener listener = new Listener(new ObjectInputStream(socket.getInputStream()));
             Thread listenerThread = new Thread(listener);
             listenerThread.start();
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Please enter username:");
-            username = scanner.nextLine();
-            Packet packet = new Packet();
-            packet.setRequestType("SetUsername");
-            packet.setFromUsername(username);
-            outputStream.writeObject(packet);
+            scanner = new Scanner(System.in);
+
             while (true){
                 Packet packet1 = new Packet();
                 System.out.println("To(username)");
@@ -54,4 +54,44 @@ public class ClientManager {
 
     }
 
+    public void login(){
+        System.out.println("Please enter your username:");
+        username = scanner.nextLine();
+        Packet packet = new Packet();
+        packet.setRequestType("SetUsername");
+        packet.setFromUsername(username);
+        try {
+            outputStream.writeObject(packet);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("\nUsername set\nWelcome" + username + "\n");
+    }
+
+    public void createGroup(){
+        Packet packet = new Packet();
+
+        packet.setRequestType("CreateGroup");
+        System.out.println("What is the groups name? ");
+        packet.setToUsername(scanner.nextLine());
+        System.out.println("What is the groups description? (write it in one line)");
+        packet.setDescription(scanner.nextLine());
+        System.out.println("Who are the members of group? (write ! in an empty line to finish)");
+        ArrayList<String> members = new ArrayList<>();
+        while (true){
+            String member = scanner.nextLine();
+            if (member.equals("!")){
+                break;
+            }
+            members.add(member);
+        }
+        packet.setMembers(members);
+
+        try {
+            outputStream.writeObject(packet);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("\nGroup Creation Request Sent\n");
+    }
 }
